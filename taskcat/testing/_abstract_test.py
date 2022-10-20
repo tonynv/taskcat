4f782b0fce75bab9f10cbeeee0e30543bc2c0fcb
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2022-10-19 21:37:13 -0500

    tweaks to get pytest to run

--- taskcat/testing/_abstract_test.py
+++ taskcat/testing/_abstract_test.py
@@ -14,48 +14,38 @@ class Test(ABC):
     """
 
     @property  # type: ignore
-    @abstractmethod
     def config(self) -> Config:
         """Get or set the current Test configuration."""
 
     @config.setter  # type: ignore
-    @abstractmethod
     def config(self, config: Config) -> None:
         pass
 
     @property  # type: ignore
-    @abstractmethod
     def passed(self) -> bool:
         """Get the result of the current test. Defaults to False if test hasn't run."""
 
     @passed.setter  # type: ignore
-    @abstractmethod
     def passed(self, new_value: bool) -> None:
         pass
 
     @property  # type: ignore
-    @abstractmethod
     def result(self) -> Any:
         """Get the result of the Test. Defaults to None if test hasn't run."""
 
     @result.setter  # type: ignore
-    @abstractmethod
     def result(self, new_value: Any) -> None:
         pass
 
-    @abstractmethod
     def __enter__(self) -> Any:
         pass
 
-    @abstractmethod
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
         pass
 
-    @abstractmethod
     def run(self) -> None:
         """Run the Test."""
 
-    @abstractmethod
     def clean_up(self) -> None:
         """Clean up after the Test."""
 
@@ -70,7 +60,6 @@ class Test(ABC):
         pass
 
     @classmethod
-    @abstractmethod
     def from_dict(
         cls: Type[T],
         input_config: dict,
